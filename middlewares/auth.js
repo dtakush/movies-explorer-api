be9602ea46056812c62340c67e9ed93ea988ff4b
Daria Takush <dashatakush@yandex.ru>
2021-09-11 16:55:02 +0300

    level-4/auth

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,25 +2,23 @@ const jwt = require('jsonwebtoken');
 
 const Unauthorized = require('../errors/Unauthorized');
 
-const { JWT_SECRET = 'super-strong-secret' } = process.env;
-
-// eslint-disable-next-line
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
-
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    throw new Unauthorized('Необходима авторизация');
+    throw new Unauthorized('Небходима авторизация');
   }
 
   const token = authorization.replace('Bearer ', '');
+  const { NODE_ENV, JWT_SECRET } = process.env;
   let payload;
 
   try {
-    payload = jwt.verify(token, JWT_SECRET);
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
   } catch (err) {
-    throw new Unauthorized('Необходима авторизация');
+    throw new Unauthorized('Небходима авторизация');
   }
 
   req.user = payload;
+
   next();
 };
