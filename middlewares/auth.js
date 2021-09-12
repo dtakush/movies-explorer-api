36ef7ce22af11c634b2aa8c83d6ac9f2ff0936ab
Daria Takush <dashatakush@yandex.ru>
2021-09-12 10:34:24 +0300

    level-4/cors'

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,6 +2,8 @@ const jwt = require('jsonwebtoken');
 
 const Unauthorized = require('../errors/Unauthorized');
 
+const { JWT_SECRET = 'super-strong-secret' } = process.env;
+
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
   if (!authorization || !authorization.startsWith('Bearer ')) {
@@ -9,11 +11,10 @@ module.exports = (req, res, next) => {
   }
 
   const token = authorization.replace('Bearer ', '');
-  const { NODE_ENV, JWT_SECRET } = process.env;
   let payload;
 
   try {
-    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
+    payload = jwt.verify(token, JWT_SECRET);
   } catch (err) {
     throw new Unauthorized('Небходима авторизация');
   }
